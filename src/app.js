9d88586e756fa76e94e03c46b2e601cfdb79ae1b
Debanjan Saha <debanjansaha1234@gmail.com>
2022-01-09 15:07:01 +0530

    Adding Frontend

--- src/app.js
+++ src/app.js
@@ -7,12 +7,14 @@ const Register=require("./model/registration");
 const port=process.env.PORT || 8000;
 const hbs=require("hbs");
 const res = require("express/lib/response");
-const req = require("express/lib/request");
+const req = require("express/lib/request")
 
 const viewPath=path.join(__dirname,"../src/views");
+const dirPath=path.join(__dirname,"../src/public");
 
 app.set("view engine","hbs");
 app.set("views",viewPath);
+app.use(express.static(dirPath));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
@@ -20,6 +22,12 @@ app.get("/",(req,res)=>{
     res.render("index.hbs");
 });
 
+app.get("/backend",(req,res)=>{
+    res.render("backend.hbs");
+});
+app.get("/frontend",(req,res)=>{
+    res.render("frontend.hbs");
+});
 app.post("/signup",async(req,res)=>{
     try{
         const userData= new Register({
@@ -27,9 +35,9 @@ app.post("/signup",async(req,res)=>{
             Contact:req.body.Contact
         })
         const Data= await userData.save();
-        res.status(201).send("Congraluations you just got enrolled.");
+        res.status(201).send("<h2>Congraluations you just got enrolled.</h2>");
     }catch(e){
-        res.status(404).send(e);
+        res.status(404).send("<h2>Error: Something went wrong Try again</h2>");
     }
   
 })
