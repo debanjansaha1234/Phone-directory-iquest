d4cd62057df9c55a2db62197dcf42221b1f126ea
debanjansaha1234 <76622272+debanjansaha1234@users.noreply.github.com>
2022-01-20 15:51:40 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,12 +1,4 @@
 const mongoose=require("mongoose");
-
-// mongoose.connect("mongodb://localhost:27017/Students")
-// .then(()=>{
-//     console.log("Connection successfull");
-// })
-// .catch((e)=>{
-//     console.log(e);
-// })
 mongoose.connect("mongodb+srv://Debanjan:Debanjan1234@cluster0.2bjz9.mongodb.net/Student?retryWrites=true&w=majority")
 .then(()=>{
     console.log("Connection Established");
